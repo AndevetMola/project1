52b1b378c439d51b0471a4c4401c7dc93f805bd7
AndevetMola <Andevet.Mola@s.afeka.ac.il>
2025-04-03 11:17:35 +0300

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello  world!");
+    }
+
+      public static void get(String[] args) {
         System.out.println("Hello another world!");
     }
 }
